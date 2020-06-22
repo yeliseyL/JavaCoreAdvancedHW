5ebd56dcda23651db11454253bf4b43b7b38ff1a
Elisey Lobanov <crimsonorama@gmail.com>
2020-06-22 22:50:52 +0300

    hw3

--- src/HW3/Main.java
+++ src/HW3/Main.java
@@ -1,4 +1,34 @@
 package HW3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<String> arr = new ArrayList<>(Arrays.asList("cat", "dog", "cat",
+                "tiger", "cougar", "giraffe", "cat", "bird", "dog", "bird"));
+
+        countElements(arr);
+
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Ivanov", "1111111");
+        phoneBook.add("Petrov", "2222222", "3333333");
+        phoneBook.add("Sidorov", "4444444", "5555555");
+        phoneBook.add("Ivanov", "6666666", "7777777");
+        phoneBook.get("Ivanov");
+        phoneBook.get("Petrov");
+        phoneBook.get("Sidorov");
+
+    }
+
+    public static void countElements(List<String> list) {
+        Map<String, Long> count = list.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        count.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
 }
--- src/HW3/PhoneBook.java
+++ src/HW3/PhoneBook.java
@@ -1,4 +1,29 @@
 package HW3;
 
+import java.util.*;
+
 public class PhoneBook {
+
+    private Map<String, ArrayList<String>> book;
+
+    public PhoneBook() {
+        this.book = new HashMap<>();
+    }
+
+    public void add(String name, String ... numbers) {
+        ArrayList<String> newNumbers = new ArrayList<>(Arrays.asList(numbers));
+        if (book.containsKey(name)) {
+            book.get(name).addAll(newNumbers);
+        } else {
+            book.put(name, newNumbers);
+        }
+    }
+
+    public void get(String name) {
+        if (book.containsKey(name)) {
+            System.out.printf("%s: ", name);
+            book.get(name).forEach(e -> System.out.printf("%s ", e));
+            System.out.println();
+        }
+    }
 }
