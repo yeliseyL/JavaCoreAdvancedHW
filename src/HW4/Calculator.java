404c3fe1ab4e75d4337acfa62e01329abb075548
Elisey Lobanov <crimsonorama@gmail.com>
2020-06-26 02:45:04 +0300

    hw4 start

---
+++ src/HW4/Calculator.java
@@ -0,0 +1,11 @@
+package HW4;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class Calculator extends Application {
+
+}
---
+++ src/HW4/Controller.java
@@ -0,0 +1,10 @@
+package HW4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+}
