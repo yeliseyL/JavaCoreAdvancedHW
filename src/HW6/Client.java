a804f27856dc09684aa5ae89a93bb3e38acfbd3a
Elisey Lobanov <crimsonorama@gmail.com>
2020-07-02 23:19:47 +0300

    hw6

--- src/HW6/Client.java
+++ src/HW6/Client.java
@@ -1,4 +1,63 @@
 package HW6;
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client{
+
+    public static void main(String[] args) {
+        final int PORT = 8806;
+        final String IP_ADDRESS = "localhost";
+        Socket socket;
+        DataOutputStream out;
+        DataInputStream in;
+        Scanner input  = new Scanner(System.in);
+
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        System.out.println("Server: " + str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        out.writeUTF(input.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/HW6/Server.java
+++ src/HW6/Server.java
@@ -1,4 +1,47 @@
 package HW6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) {
+        final int PORT = 8806;
+
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Server is running");
+
+            try (Socket socket = server.accept()) {
+                System.out.println("Client connected");
+
+                try (Scanner input = new Scanner(System.in);
+                     DataInputStream in = new DataInputStream(socket.getInputStream());
+                     DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+
+                    new Thread(() -> {
+                        try {
+                            while (true) {
+                                out.writeUTF(input.nextLine());
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        System.out.println("Client: " + str);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
